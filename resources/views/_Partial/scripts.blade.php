9f97fe102550bd3362d0c8002a67740bcabaeaf5
ManuelGSk8 <mgoicochea@MacBook-Pro-de-Manuel.local>
2015-08-14 04:03:07 -0500

    commit

---
+++ resources/views/_Partial/scripts.blade.php
@@ -0,0 +1,13 @@
+<!-- jQuery 2.1.4 -->
+<script src="{{URL::asset('assets/scripts/plugins/jQuery/jQuery-2.1.4.min.js')}}" type="text/javascript"></script>
+<!-- Bootstrap 3.3.2 JS -->
+<script src="{{URL::asset('assets/scripts/bootstrap/js/bootstrap.min.js')}}" type="text/javascript"></script>
+<!-- date-range-picker
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.2/moment.min.js" type="text/javascript"></script>
+
+<script src="{{URL::asset('assets/scripts/plugins/daterangepicker/daterangepicker.js')}}" type="text/javascript"></script>
+-->
+
+<!-- Date Picker Bootstrap -->
+<script src="{{URL::asset('assets/scripts/plugins/bootstrap-datepicker/js/bootstrap-datepicker.min.js')}}" type="text/javascript"></script>
